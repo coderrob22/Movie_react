89ced8b3d6475fb719d058bf645ebeb064199f30
coderrob22 <robertknott7@gmail.com>
2025-10-09 15:08:33 -0400

    hover effect for Star component

--- movies/src/StarRating.js
+++ movies/src/StarRating.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const containerStyle ={
     display: "flex",
     alignItems: "center",
@@ -16,7 +18,8 @@ const textStyle ={
 
 // Setting MaxRating to 5 gives the default rating for the prop so that if no prop is passed to the reusable component, then it will atleast default to 5.
 export default function StarRating ({maxRating = 5}){
-    const [rating, setRating] = useState(1);
+    const [rating, setRating] = useState(0);
+    const [tempRating, setTempRating] = useState(0);
 
     function handleRating(rating){
         setRating(rating);
@@ -26,11 +29,16 @@ export default function StarRating ({maxRating = 5}){
         <div style={containerStyle}>
             <div style={starContainerStyle}>
                 {Array.from({ length: maxRating}, (_,i) =>(
-                    <Star key={i} onRate={() => handleRating(i+1)} 
-                    full = {rating >= i + 1}/>
+                    <Star 
+                        key={i} 
+                        onRate={() => handleRating(i+1)} 
+                        full = {tempRating ? tempRating >= i + 1 : rating >= i + 1}
+                        onHoverIn = {() => setTempRating(i+1)}
+                        onHoverOut ={()=> setTempRating(0)}
+                    />
                     ))}
             </div>
-            <p style={textStyle}>{rating || ""}</p>
+            <p style={textStyle}>{tempRating || rating || ""}</p>
         </div>
     );
 }
@@ -42,11 +50,17 @@ const starStyle ={
     cursor: "pointer"
 }
 
-function Star({onRate, full}){
+function Star({onRate, full, onHoverIn, onHoverOut}){
     return(
-        <span role="button" style={starStyle} onClick={onRate}>
-                full ? (
-                {<svg
+        <span 
+            role="button" 
+            style={starStyle} 
+            onClick={onRate} 
+            onMouseEnter={onHoverIn}
+            onMouseLeave={onHoverOut}
+        >
+                {full ? (
+                <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                     fill="#000"
@@ -55,7 +69,7 @@ function Star({onRate, full}){
                 <path
                     d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.     364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.     07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
                 />
-                </svg>}
+                </svg>
                 ) : (
                     <svg
   xmlns="http://www.w3.org/2000/svg"
@@ -71,7 +85,7 @@ function Star({onRate, full}){
   />
 </svg>
                 )
-        </span>
+            }</span>
     )
 }
 
